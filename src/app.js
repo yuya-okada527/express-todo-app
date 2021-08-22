f3496ab77906c5509d05e8225755b710d2efd3fc
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-22 15:16:32 +0900

    add mongodb

--- src/app.js
+++ src/app.js
@@ -1,36 +1,64 @@
 const express = require("express");
+const mongodb = require("mongodb");
 const app = express();
+const MongoClient = mongodb.MongoClient;
 
 app.use(express.json())
 
 app.listen(3000);
 
-const todos = [
-  {id: 1, title: "Coding", done: 1},
-  {id: 2, title: "Test", done: 0}
-]
+const MONGO_URL = "mongodb://root:example@localhost:27017"
 
 app.get("/", (req, res) => {
-  const data = {
-    todos: todos
-  }
-  res.render(`${__dirname}/public/index.ejs`, data);
+  let todos = []
+  MongoClient.connect(MONGO_URL, (err, client) => {
+    const db = client.db("db");
+    db.collection("todos").find().toArray((err, docs) => {
+      if (!docs) {
+        return;
+      }
+      for (const doc of docs) {
+        todos.push({
+          id: doc.id,
+          title: doc.title,
+          done: doc.done
+        });
+      }
+      const data = {
+        todos: todos
+      }
+      res.render(`${__dirname}/public/index.ejs`, data);
+    })
+  })
 })
 
 app.post("/todos", (req, res) => {
-  const todo = {
-    id: todos.length + 1,
-    title: req.body.title,
-    done: 0
-  }
-  todos.push(todo)
-  res.send("success")
+  MongoClient.connect(MONGO_URL, async (err, client) => {
+    const db = client.db("db");
+    const count = await db.collection("todos").find().count();
+    const todo = {
+      id: count + 1,
+      title: req.body.title,
+      done: 0
+    }
+    db.collection("todos").insertOne(todo, (err, result) => {
+      res.send("success")
+    })
+  })
 })
 
 app.put("/todos", (req, res) => {
-  for (let todo of todos) {
-    if (todo.id != req.body.id) continue;
-    todo.done = req.body.done
-  }
-  res.send("success")
+  console.log(req.body.id)
+  MongoClient.connect(MONGO_URL, (err, client) => {
+    const db = client.db("db");
+    db.collection("todos").find({id: Number.parseInt(req.body.id)}).toArray((err, result) => console.log(result))
+    db.collection("todos").updateOne(
+      { id: Number.parseInt(req.body.id)},
+      { $set: { done: Number.parseInt(req.body.done) }},
+      (err, result) => {
+        console.log(result);
+        res.send("success")
+      }
+    )
+  })
 })
