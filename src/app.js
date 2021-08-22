2e1875c95e11381a2150f9d70eb68ab23afbb644
yuya-okada527 <yuya.okada527@gmail.com>
2021-08-22 13:36:54 +0900

    add ejs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000);
 
+const todos = [
+  {id: 1, title: "Coding", done: 1},
+  {id: 2, title: "Test", done: 0}
+]
+
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  const data = {
+    todos: todos
+  }
+  res.render(`${__dirname}/public/index.ejs`, data);
+})
+
+app.post("/todos", (req, res) => {
+  console.log(req.body);
+  res.send("POST")
 })
